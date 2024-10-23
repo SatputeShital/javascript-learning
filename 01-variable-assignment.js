3f85f7f98117fd762f09200c17aaf4fddacc9d13
Shital Satpute <satputeshitald@gmail.com>
2024-10-23 15:12:23 +0530

    Add variable Assignment files
    Add variable Assignment files

---
+++ 01-variable-assignment.js
@@ -0,0 +1,26 @@
+//city variable declaration
+var city;
+//variable city Initialization
+city="Pune";
+console.log("City Name:",city);
+// console.log("----------------------------");
+
+var bestFriend="Renu";//variable declaration and Initialization
+console.log("Best Friend Name:",bestFriend);
+// console.log("----------------------------");
+
+var country="India";//variable declaration and Initialization
+console.log("Country Name:",country);
+// console.log("----------------------------");
+
+var school="S.G. Vidyalaya";//variable declaration and Initialization
+console.log("School Name:",school);
+// console.log("----------------------------");
+
+var college="SSGM College";//variable declaration and Initialization
+console.log("College Name:",college);
+// console.log("----------------------------");
+
+
+
+
---
+++ 01-variableAssign.js
@@ -0,0 +1,47 @@
+
+//Declare a variable with name → myFullName
+var myFullName;
+
+//Log the variable myFullName value before initialization on console
+console.log("My full Name:",myFullName);
+console.log("----------------------------");
+
+//Initialize the myFullName variable with your first and last name only. 
+myFullName="Shital Satpute";
+
+//Log the value on console
+console.log("My Name:",myFullName);
+
+//Update the myFullName variable with your first name, middle name and last name. Log the updated value on console
+myFullName="Shital Dattatray Satpute";
+//Update the myFullName variable with your first name, middle name and last name. Log the updated value on console
+console.log("My Full name:",myFullName);
+
+console.log("----------------------------");
+//Declare and initialize a variable with name 'myLove' with value "I love only JS" and log result on console 
+var mylove="I love only JS";
+console.log(mylove);
+
+console.log("----------------------------");
+
+// Declare and initialize must be on same line
+var firstname="Shital";
+var lastname="Satpute";
+var age=29;
+var address="Pune";
+var pincode=411033;
+var gender="female";
+var isMarried="True";
+console.log("First Name:",firstname);
+console.log("Last Name:",lastname);
+console.log("Age:",age);
+console.log("Address:",address);
+console.log("Pincode:",pincode);
+console.log("Gender:",gender);
+console.log("Is Married:",isMarried)
+
+
+
+
+
+
