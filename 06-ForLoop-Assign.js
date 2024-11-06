5ddb8eaf0c8ef0068a75ba31501447c61fad2e05
Shital Satpute <satputeshitald@gmail.com>
2024-11-06 14:34:20 +0530

    Add SwitchCase and Forloop files
    Add SwitchCase and Forloop files

---
+++ 06-ForLoop-Assign.js
@@ -0,0 +1,51 @@
+console.log(`====Print Numbers from 5 to 15 by Incrementing By 1==== `);
+for (var index=5; index<=15; index++) 
+    {
+       console.log(index);
+    }
+
+console.log(`====Print Numbers from 50 to 40 by Decrementing by 1==== `);
+for (var index = 50; index>=40; index--) 
+    {
+       console.log(index);
+    }
+
+console.log(`====Print first 15 Odd Numbers==== `);   
+for (var index=1; index<=30; index=index+2) 
+   {
+       console.log(index);
+  }
+// for (let i = 1, count = 0; count < 15; i += 2, count++) {
+//    console.log(i);
+// }
+
+
+//   let oddNumbers = [];
+// for (let i = 1; oddNumbers.length < 15; i += 2) {
+//   oddNumbers.push(i);
+// }
+// console.log(oddNumbers);
+console.log(`====Print first 10 Even Numbers==== `);   
+for (var index = 2; index <=20 ; index+=2)
+   {
+      console.log(index);
+      
+   }
+console.log(`====Print Table of 5==== `); 
+for (var index = 1; index <=10; index++)
+   {
+      console.log(index*5);
+   } 
+console.log(`====Print Table of 10==== `); 
+for (var index = 1; index <=10; index++) 
+   {
+      console.log(index*10);     
+   }
+
+console.log(`====Print Table of 10 in Reverse Order==== `); 
+for (var index = 10; index >=1; index--) 
+   {
+      console.log(index*10);     
+   }
+
+
